cb93a0bd07cb6ba8b37ea6652a174a5887211542
Mario Martinez <mmartinez@energy-tools.net>
2018-10-02 14:05:21 +0200

    serialVersionUID randomly generated by IDEA

--- highcharts-wrapper/src/main/java/com/googlecode/wickedcharts/highcharts/options/series/DataGrouping.java
+++ highcharts-wrapper/src/main/java/com/googlecode/wickedcharts/highcharts/options/series/DataGrouping.java
@@ -23,7 +23,7 @@ import java.util.ArrayList;
  */
 public class DataGrouping implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 7994230131347680735L;
 
     /**
      * The method of approximation inside a group.
