15f2748b3fa2a9de625934081314ee967850814e
xtoro <xtoro@energy-tools.net>
2019-02-26 11:46:46 +0100

    add renamed includedInCSVExport by includedInDataExport

--- highcharts-wrapper/src/main/java/com/googlecode/wickedcharts/highcharts/options/NavigatorSeries.java
+++ highcharts-wrapper/src/main/java/com/googlecode/wickedcharts/highcharts/options/NavigatorSeries.java
@@ -2,6 +2,7 @@ package com.googlecode.wickedcharts.highcharts.options;
 
 public class NavigatorSeries {
     private boolean includeInCSVExport;
+    private boolean includeInDataExport;
 
     public boolean isIncludeInCSVExport() {
         return includeInCSVExport;
@@ -9,6 +10,15 @@ public class NavigatorSeries {
 
     public NavigatorSeries setIncludeInCSVExport(boolean includeInCSVExport) {
         this.includeInCSVExport = includeInCSVExport;
+        return this;
+    }
+
+    public boolean isIncludeInDataExport() {
+        return includeInDataExport;
+    }
+
+    public NavigatorSeries setIncludeInDataExport(boolean includeInDataExport) {
+        this.includeInDataExport = includeInDataExport;
         return this;
     }
 }
